b242ca339f37728e3cda300f83015279f630cf9f
kstukenborg <@stuk>
2018-06-20 12:59:18 -0700

    Initial Commit

---
+++ git tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git tutorial
+//
+//  Created by Kathleen Stukenborg on 6/20/18.
+//  Copyright © 2018 Kathleen Stukenborg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
